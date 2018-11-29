29401eb7931d04db4c25a4f5c2712981dd1768bb
David Howe <umbredave@gmail.com>
2018-11-29 13:55:28 +0200

    - FIRST COMMIT

---
+++ app/src/main/java/com/hearxgroup/mhealthcsprolauncher/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.hearxgroup.mhealthcsprolauncher
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        try {
+            startActivity(
+                getLaunchAppIntent(
+                    getPackageManager(),
+                    null,
+                    "com.hearxgroup.mhealthstudio.intent.action.testrequestfromfile"
+                )
+            )
+        } catch(error: NullPointerException) {
+            Toast.makeText(this, "Missing valid mHealth app!", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    fun getLaunchAppIntent(packageManager: PackageManager, bundle: Bundle?, actionName: String): Intent? {
+        val newIntent = Intent(actionName)
+        newIntent.type = "text/plain"
+        val activities = packageManager.queryIntentActivities(newIntent, 0)
+        val isIntentSafe = activities.size > 0
+        if (isIntentSafe) {
+            if (bundle != null) {
+                newIntent.putExtras(bundle)
+            }
+
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            newIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            return newIntent
+        } else {
+            return null
+        }
+    }
+}
