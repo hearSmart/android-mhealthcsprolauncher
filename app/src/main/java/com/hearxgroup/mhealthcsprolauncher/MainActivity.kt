0878dbecd112cacf7d9f3a5ddd2bae2568e88adb
David Howe <umbredave@gmail.com>
2019-01-07 13:26:18 +0200

    - Reduced min api to 19, Release v1002

--- app/src/main/java/com/hearxgroup/mhealthcsprolauncher/MainActivity.kt
+++ app/src/main/java/com/hearxgroup/mhealthcsprolauncher/MainActivity.kt
@@ -3,10 +3,8 @@ package com.hearxgroup.mhealthcsprolauncher
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
